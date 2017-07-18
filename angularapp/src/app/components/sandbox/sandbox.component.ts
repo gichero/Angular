f565e7a0a67a21495f6d99cb926726d8ddc8e781
Amos <amosgathungu@gmail.com>
2017-07-18 15:14:54 -0400

    Loop data with ngFor directive

--- angularapp/src/app/components/sandbox/sandbox.component.ts
+++ angularapp/src/app/components/sandbox/sandbox.component.ts
@@ -2,10 +2,48 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    templateUrl: `./sandbox.component.html`,
-    styleUrls: ['./sandbox.component.css']
+    template: `
+            <h1>Welcome!</h1>
+            {{people}}
+            <ul>
+                <li *ngFor="let person of people">
+                    {{person}}
+                </li>
+            </ul>
+            <ul>
+                <li *ngFor="let person of people; let i = index">
+                    {{i}} {{person}}
+                </li>
+            </ul>
+            <ul>
+                <li *ngFor="let person of people2">
+                    {{person.firstName}} {{person.lastName}}
+                </li>
+            </ul>
+    `
+
+    ,
+
 })
 
 export class SandboxComponent{
+    people = ['Sidney', ' Tess', ' Emilly', ' Brenda'];
 
+    people2 = [
+        {firstName: 'Sidney',
+         lastName: 'Nzioka'},
+
+        {firstName: 'Tess',
+         lastName: 'Njakwe'},
+
+        {firstName: 'Emilly',
+         lastName: 'Gesare'},
+
+        {firstName: 'Brenda',
+         lastName: 'Akinyi'}
+    ];
+
+    constructor(){
+        this.people[2] = "Emily";
+    }
 }
