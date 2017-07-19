fe4167cf173d4525a94dc28ee7d7f333e1b7eb5c
Amos <amosgathungu@gmail.com>
2017-07-19 16:11:40 -0400

    using pipes

--- angularapp/src/app/components/sandbox/sandbox.component.ts
+++ angularapp/src/app/components/sandbox/sandbox.component.ts
@@ -2,37 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    template: `
-            <h1>Welcome!</h1>
-            <h2 [class.special]="isSpecial">This class binding is special</h2>
-            <h2 [ngClass]="currentClasses">This div is initially special and savable</h2>
-            `,
-            styles:[
-                `
-                    .special{
-                        color: purple
-                    }
-                    .savable{
-                        text-transform: uppercase
-                    }
-                `
-            ]
+    template: `<h1>Welcome!</h1>
+                <p>My birthday is on {{birthday | date |uppercase}}</p>
+                <p>My birthday is on {{birthday | date: "MM-dd-yyyy"}}</p>
+                <p>My birthday in {{birthday | date: "yyyy"}}</p>
+                <p>I listen to {{'audio books' | uppercase}}</p>
+                <p>I hate {{'REALITY TV' | lowercase}}</p>
+                <p>Your total is {{total | currency:"GBP":"1"}}</p>
+                <p>Your fee is {{fee | percent}}</p>
+    `
 
 })
 
 export class SandboxComponent{
-    isSpecial:boolean = true;
-    canSave:boolean = true;
-    currentClasses = {};
-
-        constructor(){
-            this.setCurrentClasses();
-        }
-
-        setCurrentClasses(){
-            this.currentClasses = {
-                savable:this.canSave,
-                special:this.isSpecial
-            }
-        }
+    birthday = new Date(1976, 8, 8);
+    total:number = 500;
+    fee:number = 0.2;
     }
