42bf1b6bdfba699790bde6a6dc1511cb6c5b115e
Amos <amosgathungu@gmail.com>
2017-07-23 15:29:18 -0400

    Mouse events

--- angularapp/src/app/components/sandbox/sandbox.component.ts
+++ angularapp/src/app/components/sandbox/sandbox.component.ts
@@ -3,19 +3,25 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'sandbox',
     template: `<h1>Welcome!</h1>
-                <p>My birthday is on {{birthday | date |uppercase}}</p>
-                <p>My birthday is on {{birthday | date: "MM-dd-yyyy"}}</p>
-                <p>My birthday in {{birthday | date: "yyyy"}}</p>
-                <p>I listen to {{'audio books' | uppercase}}</p>
-                <p>I hate {{'REALITY TV' | lowercase}}</p>
-                <p>Your total is {{total | currency:"GBP":"1"}}</p>
-                <p>Your fee is {{fee | percent}}</p>
+                <button id = "btn" (click)="fireEvent($event)">Click Event</button>
+                <br>
+                <button id = "btn" (mouseover)="fireEvent($event)">MouseOver Event</button>
+                <br>
+                <button id = "btn" (mousedown)="fireEvent($event)">MouseDowm Event</button>
+                <br>
+                <button id = "btn" (mouseup)="fireEvent($event)">MouseUp Event</button>
+                <br>
+                <button id = "btn" (dblclick)="fireEvent($event)">DoubleClick</button>
+                <br>
+                <button id = "btn" (drag)="fireEvent($event)">Drag Event</button>
+                <br>
+                <button id = "btn" (dragover)="fireEvent($event)">DragOver Event</button>
     `
 
 })
 
 export class SandboxComponent{
-    birthday = new Date(1976, 8, 8);
-    total:number = 500;
-    fee:number = 0.2;
+    fireEvent(e){
+        console.log(e.type);
+    }
     }
