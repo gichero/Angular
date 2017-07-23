727d079bfab6e4b1e52833ddf20726de04d134a0
Amos <amosgathungu@gmail.com>
2017-07-23 15:42:34 -0400

    Changing properties with events

--- angularapp/src/app/components/sandbox/sandbox.component.ts
+++ angularapp/src/app/components/sandbox/sandbox.component.ts
@@ -3,25 +3,20 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'sandbox',
     template: `<h1>Welcome!</h1>
-                <button id = "btn" (click)="fireEvent($event)">Click Event</button>
-                <br>
-                <button id = "btn" (mouseover)="fireEvent($event)">MouseOver Event</button>
-                <br>
-                <button id = "btn" (mousedown)="fireEvent($event)">MouseDowm Event</button>
-                <br>
-                <button id = "btn" (mouseup)="fireEvent($event)">MouseUp Event</button>
-                <br>
-                <button id = "btn" (dblclick)="fireEvent($event)">DoubleClick</button>
-                <br>
-                <button id = "btn" (drag)="fireEvent($event)">Drag Event</button>
-                <br>
-                <button id = "btn" (dragover)="fireEvent($event)">DragOver Event</button>
+                <button (click)="changeValue()">Change Value</button>
+                <div *ngIf="value">
+                    <h1>{{ text }}</h1>
+                </div>
     `
 
 })
 
 export class SandboxComponent{
-    fireEvent(e){
-        console.log(e.type);
+    text:string = 'Hello World';
+    value:boolean = true;
+    changeValue(){
+        //this.value = false;
+        this.value = !this.value;
+
     }
     }
