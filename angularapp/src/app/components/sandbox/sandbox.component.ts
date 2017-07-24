da32554bc0c9be163689772a1e147e3299898406
Amos <amosgathungu@gmail.com>
2017-07-24 14:44:01 -0400

    form submission

--- angularapp/src/app/components/sandbox/sandbox.component.ts
+++ angularapp/src/app/components/sandbox/sandbox.component.ts
@@ -3,26 +3,35 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'sandbox',
     template: `<h1>Welcome!</h1>
-                <form>
+                <form (submit)="onSubmit()">
                 <div class = "container">
                     <div class = "form-group">
                         <label>Name</label>
                         <input type = "text" [(ngModel)]="name" name="name" class = "form-control">
                     </div>
-                    <div class = "form-group">
-                        <label>Age</label>
-                        <input type = "number" [(ngModel)]="age" name="age" class = "form-control">
-                    </div>
+
                         <input type = "submit" value = "submit" class = "btn btn-success">
                 </div>
                 </form>
                 <h4>Name: {{name}}</h4>
-                <h4>Age: {{age}}</h4>
+                <hr>
+                <div>
+                <ul class= "list-group">
+                    <li class="list-group-item" *ngFor="let user of users">
+                    {{ user }}</li>
+                </ul>
+                </div>
+
     `
 
 })
 
 export class SandboxComponent{
     name: string = "";
-    age: number = 0;
+    users:string []= ['Vinnie Paz', 'Jane Austen', 'Amber Rose'];
+
+    onSubmit(){
+        this.users.push(this.name);
+        this.name = "";
+    }
     }
