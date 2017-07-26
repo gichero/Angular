37d488f85668a1fa86c4a726ea066174fc56bc3c
Amos <amosgathungu@gmail.com>
2017-07-26 13:49:43 -0400

    http module get

--- angularapp/src/app/components/sandbox/sandbox.component.ts
+++ angularapp/src/app/components/sandbox/sandbox.component.ts
@@ -4,22 +4,27 @@ import { DataService } from '../../services/data.service';
 @Component({
     selector: 'sandbox',
     template: `<h1>Welcome!</h1>
-    <ul class = "list-group">
-        <li class = "list-group-item" *ngFor="let d of data">{{d}}
-        </li>
-    </ul>
-
+            <div *ngFor="let user of users">
+                <div class = "well">
+                    <ul class = "list-group">
+                        <li class = "list-group-item">Name: {{user.name}}</li>
+                        <li class = "list-group-item">Email: {{user.email}}</li>
+                        <li class = "list-group-item">Phone: {{user.phone}}</li>
+                    </ul>
+                    <br>
+                </div>
+            </div>
     `
 
 })
 
 export class SandboxComponent{
-    data:any[] = [];
+    users:any[];
 
     constructor(public dataService:DataService){
-        this.dataService.getData().subscribe(data => {
-            //console.log(data);
-            this.data.push(data);
+        this.dataService.getUsers().subscribe(users => {
+            //console.log(users);
+            this.users = users;
         });
     }
 }
