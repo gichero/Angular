340d6789d6e2ff6c1c863c7bc60589be3f9bafe0
Amos <amosgathungu@gmail.com>
2017-07-26 22:18:05 -0400

    http module post

--- angularapp/src/app/components/sandbox/sandbox.component.ts
+++ angularapp/src/app/components/sandbox/sandbox.component.ts
@@ -4,6 +4,25 @@ import { DataService } from '../../services/data.service';
 @Component({
     selector: 'sandbox',
     template: `<h1>Welcome!</h1>
+            <form (submit)="onSubmit()">
+                <div class = "form-group">
+                    <label>Name</label>
+                    <input type = "text" class="form-control" [(ngModel)]
+                    ="user.name" name="name">
+                </div>
+                <div class = "form-group">
+                    <label>Email</label>
+                    <input type = "text" class="form-control" [(ngModel)]
+                    ="user.email" name="email">
+                </div>
+                <div class = "form-group">
+                    <label>Phone</label>
+                    <input type = "text" class="form-control" [(ngModel)]
+                    ="user.phone" name="phone">
+                </div>
+                <input type = "submit" class = "btn btn-success" value="Submit">
+            </form>
+            <hr>
             <div *ngFor="let user of users">
                 <div class = "well">
                     <ul class = "list-group">
@@ -20,11 +39,22 @@ import { DataService } from '../../services/data.service';
 
 export class SandboxComponent{
     users:any[];
+    user = {
+        name:'',
+        email:'',
+        phone:''
+    }
 
     constructor(public dataService:DataService){
         this.dataService.getUsers().subscribe(users => {
             //console.log(users);
             this.users = users;
         });
+    }
+    onSubmit(){
+        this.dataService.addUser(this.user).subscribe(user => {
+            console.log(user);
+            this.users.unshift(user);
+        })
     }
 }
